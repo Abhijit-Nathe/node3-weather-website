d109eec147a54e934ed5936cc686ab5e61f6eb7f
Abhijit Nathe <abhijitnathe@Abhijits-MBP.mshome.net>
2023-04-09 16:12:52 +0530

    remove unecessary console log commnad

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,5 @@
 // const { response } = require("express");
 
-console.log("client side javascript is loaded ");
-
 // fetch("http://puzzle.mead.io/puzzle").then((response) => {
 //   response.json().then((data) => {
 //     console.log(data);
